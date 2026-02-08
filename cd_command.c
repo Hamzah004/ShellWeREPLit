b59e6a6fa2f4ae5048be5008214d1bfc517b2a2d
Abdallah Mufleh <amufleh@c2r4s1.42amman.com>
2026-02-08 16:43:50 +0300

    tray to implement cd command

---
+++ cd_command.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{
+        int id = fork();
+        if (id == 0)
+        {
+                chdir("/home/amufleh/core");
+                int fd = open("file_in_anoter_dir.txt",O_CREAT | O_WRONLY , 0644);
+                write(fd,"abdallah in another dir",8);
+        }
+        else
+        {
+                int fd = open("file_in_the_main_dir.txt",O_CREAT | O_WRONLY, 0644);
+                write(fd,"abdallah but in the main dir",28);
+        }
+}
+
