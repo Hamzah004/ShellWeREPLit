5b7fb3be0737866b463284977ac28b68d58fd8e0
Abdallah Mufleh <amufleh@c2r4s1.42amman.com>
2026-02-09 20:06:19 +0300

    add

--- cd_command.c
+++ cd_command.c
@@ -4,17 +4,17 @@
 
 int main()
 {
-        int id = fork();
-        if (id == 0)
-        {
-                chdir("/home/amufleh/core");
+        // int id = fork();
+        // if (id == 0)
+        // {
+                chdir("/home/amufleh/abdallah");
                 int fd = open("file_in_anoter_dir.txt",O_CREAT | O_WRONLY , 0644);
                 write(fd,"abdallah in another dir",8);
-        }
-        else
-        {
-                int fd = open("file_in_the_main_dir.txt",O_CREAT | O_WRONLY, 0644);
-                write(fd,"abdallah but in the main dir",28);
-        }
+        // }
+       //  else
+        // {
+          //       int fd = open("file_in_the_main_dir.txt",O_CREAT | O_WRONLY, 0644);
+            //     write(fd,"abdallah but in the main dir",28);
+        // }
 }
 
