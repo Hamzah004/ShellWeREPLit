c977c9075d8a824311f1482f367ba7ddc3a7de98
Hamzeh Bani Ata <hbani-at@c1r5s8.42amman.com>
2026-02-18 18:03:42 +0300

    removed signal handling for later

--- main.c
+++ main.c
@@ -11,22 +11,11 @@
 /* ************************************************************************** */
 
 #include "include/minishell.h"
-#include "libft/libft.h"
 #include <readline/history.h>
 #include <readline/readline.h>
-#include <signal.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
-
-void	handle_ctrl_c(int signal)
-{
-	(void)signal;
-	// if (signal == SIGINT)
-	// {
-	//
-	// }
-}
 
 static t_error	validate_arguments(int argc)
 {
@@ -37,13 +26,12 @@ static t_error	validate_arguments(int argc)
 
 int	main(int argc, char **argv, char **env)
 {
-	char				*line;
-	struct sigaction	sa;
-	t_error				err;
+	char	*line;
+	t_error	err;
 
-	sa.sa_handler = &handle_ctrl_c;
+	(void)argv;
+	(void)env;
 	err = validate_arguments(argc);
-	sigaction(SIGINT, &sa, NULL);
 	if (err != ERROR_SUCCESS)
 	{
 		print_error(err);
@@ -53,10 +41,6 @@ int	main(int argc, char **argv, char **env)
 	{
 		if (strlen(line) > 0)
 			add_history(line);
-		if (!ft_strncmp(line, "clear", ft_strlen(line)))
-		{
-			execve("/usr/bin/clear", argv, env);
-		}
 		free(line);
 	}
 	return (0);
