f97d49350f2357fae53532c1576e5fa3be295bc1
Abdallah Mufleh <amufleh@c1r4s13.42amman.com>
2026-03-04 14:06:22 +0300

    add input validation

--- Parsing/parser_main.c
+++ Parsing/parser_main.c
@@ -6,17 +6,44 @@
 /*   By: amufleh <amufleh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 09:41:13 by amufleh           #+#    #+#             */
-/*   Updated: 2026/03/03 11:41:39 by amufleh          ###   ########.fr       */
+/*   Updated: 2026/03/04 13:55:48 by amufleh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
+
+int	input_validation(char *input)
+{
+	int		i;
+	char	c;
+
+	if (!input || !input[0])
+		return (0);
+	i = 0;
+	c = 0;
+	while (input[i])
+	{
+		if ((input[i] == '\'' || input[i] == '\"'))
+		{
+			if (c == 0)
+				c = input[i];
+			else if (c == input[i])
+				c = 0;
+		}
+		i++;
+	}
+	if (c != 0)
+		return (0);
+	return (1);
+}
 
 int	fill_command_struct(char *input)
 {
 	t_tokens	*my_tokens;
 	t_commands	*my_commands;
 
+	if (!input_validation(input))
+		return (0);
 	my_tokens = NULL;
 	my_commands = new_command();
 	if (!my_commands)
@@ -30,10 +57,12 @@ int	fill_command_struct(char *input)
 	free_parser(my_commands, my_tokens, 1);
 	return (1);
 }
+ #include <readline/readline.h>
 
-int main()
+ int main()
 {
-	char *input = "< infile1.txt < infile2.txt \'l\'s -l -a | grep a >> outfile.txt | echo \"\'$HOME\'\" > infile.txt | echo \"abdallah\" -n | cat outfile \" <infile\" ";
+	char *input = " ";
+
 	fill_command_struct(input);
 	return (0);
 }
